fa32e6f59896c437f1f1c1f33f7f51b9183f7a0d
andryansyhh <andryansyhh@gmail.com>
2021-04-11 17:02:52 +0700

    import font styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './App.css'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import DetailCountry from "./components/Search/DetailCountry";
 import Search from "./components/Search/Search"
+import {Nav, Navbar} from 'react-bootstrap'
 
 //Theme dark-light
 import styled, { ThemeProvider } from 'styled-components';
@@ -14,21 +16,30 @@ import Button from "./components/Setting/Button";
 function App() {
   const [theme, buttonTheme] = useDarkMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
+ 
+ 
   return (
     <>
     <ThemeProvider theme={themeMode}>
       <Router>
-      <Button theme={theme} buttonTheme={buttonTheme} />
       <GlobalStyles/>
         <header>
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
               <div>
                 <Link to="/">ECHO-19</Link>
               </div>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
               <div>
                 <Link to="/search">Search</Link>
               </div>
-          </nav>
+              </Nav>
+              <div >
+              <Button theme={theme} buttonTheme={buttonTheme} />
+              </div>
+              </Navbar.Collapse>
+          </Navbar>
              
           
           <div>
