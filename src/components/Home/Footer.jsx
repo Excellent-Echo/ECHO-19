c1dc4d79145434a68b418df9fca9544f2f92e564
andryansyhh <andryansyhh@gmail.com>
2021-04-13 14:54:18 +0700

    add copyright logo

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
   return (
     <>
       <div className="container-fluid fixed-bottom" style={{ backgroundColor: "#343a40", padding: "10px", textAlign: "center" }}>
-          2021 - &nbsp;
+      &copy; 2021 - &nbsp;
           <strong>
             Developed by &nbsp;
             <a href="https://github.com/Excellent-Echo/ECHO-19">Echo-19</a>
